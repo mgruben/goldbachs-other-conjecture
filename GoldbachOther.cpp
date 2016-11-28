4a16db9558fa60dbd042be5e19926d7920376928
mgruben <GrubenM@gmail.com>
2016-11-28 11:59:16 -0600

    Convert binaryTwiceSquare to return boolean

--- GoldbachOther.cpp
+++ GoldbachOther.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-int binaryTwiceSquare(int num, vector<int> range) {
+bool binaryTwiceSquare(int num, vector<int> range) {
     int i = 0;
     int j = range.size() - 1;
     while (i <= j) {
         int mid = (i + j) / 2;
         long long calc = range.at(mid) * 2 * range.at(mid);
-        if (calc == num) return num;
+        if (calc == num) return true;
         else if (calc > num) j = mid - 1;
         else i = mid + 1;
     }
-    return -1;
+    return false;
 }
 
 int main() {
     vector<int> range;
     for (int i; i < 100; i++) range.push_back(i+1);
-    cout << binaryTwiceSquare(50, range);
+    cout << binaryTwiceSquare(71, range);
     return 0;
 }
