caeb3b6488355cbbc0589d0c16a444255f16e81f
mgruben <GrubenM@gmail.com>
2016-11-28 11:58:29 -0600

    Implement binary search within function space

--- GoldbachOther.cpp
+++ GoldbachOther.cpp
@@ -5,14 +5,20 @@ using namespace std;
 
 int binaryTwiceSquare(int num, vector<int> range) {
     int i = 0;
-    int j = range.size();
+    int j = range.size() - 1;
+    while (i <= j) {
+        int mid = (i + j) / 2;
+        long long calc = range.at(mid) * 2 * range.at(mid);
+        if (calc == num) return num;
+        else if (calc > num) j = mid - 1;
+        else i = mid + 1;
+    }
     return -1;
 }
 
 int main() {
     vector<int> range;
     for (int i; i < 100; i++) range.push_back(i+1);
-    for (int i: range) cout << i << endl;
-    
+    cout << binaryTwiceSquare(50, range);
     return 0;
 }
