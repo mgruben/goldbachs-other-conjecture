fb25a1399c389d1aa03be81d8de9bb2ec651d0e6
mgruben <GrubenM@gmail.com>
2016-11-28 11:43:16 -0600

    Add range for binary searching

--- GoldbachOther.cpp
+++ GoldbachOther.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int binaryTwiceSquare(int num, int range[]) {
+    return -1;
+}
+
+int main() {
+    int len = 100;
+    int range [len];
+    for (int i; i < len; i++) range[i] = i+1;
+    for (int i: range) cout << i << endl;
+    return 0;
+}
